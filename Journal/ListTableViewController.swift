63dd4b36c4acdb33a0adc7c4eb3dd56b3a0aca3d
Morgan Davison <morgan.davison@gmail.com>
2016-06-05 15:32:57 -0700

    Fix crash when changing date while settings tab visible, fixed list view scrolling under tab bar

--- Journal/ListTableViewController.swift
+++ Journal/ListTableViewController.swift
@@ -226,8 +226,9 @@ extension ListTableViewController: NSFetchedResultsControllerDelegate {
         case .Delete:
             tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
         case .Update:
-            //self.configureCell(tableView.cellForRowAtIndexPath(indexPath!)!, atIndexPath: indexPath!)
-            self.configureCell(tableView.cellForRowAtIndexPath(indexPath!) as! ListTableViewCell, atIndexPath: indexPath!)
+            if let indexPath = indexPath {
+                self.configureCell(tableView.cellForRowAtIndexPath(indexPath) as! ListTableViewCell, atIndexPath: indexPath)
+            }
         case .Move:
             tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
             tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
